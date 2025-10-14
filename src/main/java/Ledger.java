272693f4d3660db7233e8183b4c6b77a1ddc5534
Daja Miller <dajamiller96@gmail.com>
2025-10-14 09:55:47 -0700

    moved constructor to transaction class

--- src/main/java/Ledger.java
+++ src/main/java/Ledger.java
@@ -7,56 +7,16 @@ public class Ledger {
 
     private ArrayList<Transaction> list = new ArrayList<>();
 
-    // create constructor
-    public Ledger (LocalDate date, LocalTime time, String description, String vendor, double amount) {
-        this.date = date;
-        this.time = time;
-        this.description = description;
-        this.vendor = vendor;
-        this.amount = amount;
+    public void addTransaction(Transaction transaction) {}
+
+    public void showTransactions() {
+        list.forEach(transaction -> System.out.println(transaction.toString()));
     }
+
     public Ledger(){
 
     }
 
 
-    public LocalDate getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getVendor() {
-        return vendor;
-    }
-
-    public void setVendor(String vendor) {
-        this.vendor = vendor;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
 }
