70939d14aef47fdb55587f68c080011c5764c166
Daja Miller <dajamiller96@gmail.com>
2025-10-17 00:33:39 -0700

    commented out

--- src/main/java/Report.java
+++ src/main/java/Report.java
@@ -1,3 +1,20 @@
-
-public class Report {
-}
+//import java.util.Scanner;
+//
+//public class Report {
+//    Ledger ledger;
+//    private static void vendorSearch(Transaction list, Scanner scanner2 ) {
+//
+//
+//        boolean vendorFound = false;
+//
+//        for (Transaction t : list) {
+//            if (t.getVendor().equalsIgnoreCase(vendor)) {
+//                System.out.println(t);
+//                vendorFound = true;
+//            }
+//        }
+//        if (!vendorFound) {
+//            System.out.println("Sorry, there are no matching transactions for " +  vendor);
+//        }
+//    }
+//}
